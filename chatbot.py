9fff4fdfc3ffe45a61147d959467b26d83bc788c
AYUSH <ayushabhinav007@gmail.com>
2023-11-29 23:24:34 +0530

    first

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, request
 
 app=Flask(__name__)
@@ -11,7 +12,18 @@ def index():
     print(source_currency)
     print(amount)
     print(target_currency)
+    cf=fetch_conversion_factor(source_currency, target_currency)
+    final_amount=amount*cf
+    response={
+        'fulfillmentText': f"{final_amount} {target_currency}"
+    }
+    print(final_amount)
     return "hello"
-
+def fetch_conversion_factor(source, target):
+    url="https://api.currencyapi.com/v3/latest?apikey=cur_live_IjjYgZIv5eZLc8DjVkDyrqoBoFexHZEU0UR00Xld".format(source,target)
+    response=requests.get(url)
+    response=response.json()
+    print(response)
+    return response['{}_{}'.format(source,target)]
 if __name__=="__main__":
     app.run(debug=True)
